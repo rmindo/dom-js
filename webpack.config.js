048b8bcaa63cfc3e32fe8e65264f3f6fde2e2bb5
Ruel Mindo <wrelmind@gmail.com>
2020-07-03 01:30:30 +0800

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const path = require('path');
 module.exports = {
   entry: [
     '@babel/polyfill',
-    './src/Main.js'
+    './index.js'
   ],
   output: {
     path: path.resolve(__dirname, 'dist'),
