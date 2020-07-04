07ba5c4687cfd601e4087682c560cd9d64dffadb
Ruel Mindo <wrelmind@gmail.com>
2020-07-04 19:28:16 +0800

    Removed spacing

--- src/views/segments.js
+++ src/views/segments.js
@@ -2,52 +2,50 @@
 // import styles from './Styles/Users';
 
 
-
 class Segments {
 
-	constructor() {
-		this.head.title('Segments');
-	}
+  constructor() {
+    this.head.title('Segments');
+  }
 
 
-	button(node) {
-		node.id = 'button';
-		node.innerHTML = 'Click to see the world!';
-	}
+  button(node) {
+    node.id = 'button';
+    node.innerHTML = 'Click to see the world!';
+  }
 
 
+  content($) {
+    $.setAttribute('id', 'content');
 
-	content($) {
-		$.setAttribute('id', 'content');
-
-		return {
-			h1: 'Segments',
-			p: [
-				{
-					strong: 'webpack has a rich plugin interface.',
-					span: 'Test',
-					em: {
-						small: 'small',
-						span: {
-							strike: 'strike',
-							small: [
-								'one',
-								'two',
-								'three',
-								{
-									span: {
-										strong: 'four'
-									}
-								}
-							]
-						}
-					}
-				},
-				'Most of the features within webpack itself use this plugin interface.'
-			],
-			button: this.button
-		};
-	}
+    return {
+      h1: 'Segments',
+      p: [
+        {
+          strong: 'webpack has a rich plugin interface.',
+          span: 'Test',
+          em: {
+            small: 'small',
+            span: {
+              strike: 'strike',
+              small: [
+                'one',
+                'two',
+                'three',
+                {
+                  span: {
+                    strong: 'four'
+                  }
+                }
+              ]
+            }
+          }
+        },
+        'Most of the features within webpack itself use this plugin interface.'
+      ],
+      button: this.button
+    };
+  }
 }
 
 
