3207019de97aca02d6063d89c0729b1656508b9f
Ruel Mindo <wrelmind@gmail.com>
2020-07-07 00:23:11 +0800

    Removed spacing

--- src/views/reports.js
+++ src/views/reports.js
@@ -1,44 +1,43 @@
 class Reports {
 
-	constructor() {
-		this.head.title('Reports');
-	}
+  constructor() {
+    this.head.title('Reports');
+  }
 
 
-	button(node) {
-		node.id = 'button';
-		node.innerHTML = 'Click to see the world!';
-	}
+  button(node) {
+    node.id = 'button';
+    node.innerHTML = 'Click to see the world!';
+  }
 
 
-
-	content() {
-		return {
-			h1: 'Reports',
-			p: [
-				{
-					strong: 'webpack has a rich plugin interface.',
-					span: 'Test',
-					em: {
-						small: 'small',
-						span: {
-							strike: 'strike',
-							small: [
-								'one',
-								'two',
-								'three',
-								{
-									span: 'four'
-								}
-							]
-						}
-					}
-				},
-				'Most of the features within webpack itself use this plugin interface.'
-			],
-			button: this.button
-		};
-	}
+  content() {
+    return {
+      h1: 'Reports',
+      p: [
+        {
+          strong: 'webpack has a rich plugin interface.',
+          span: 'Test',
+          em: {
+            small: 'small',
+            span: {
+              strike: 'strike',
+              small: [
+                'one',
+                'two',
+                'three',
+                {
+                  span: 'four'
+                }
+              ]
+            }
+          }
+        },
+        'Most of the features within webpack itself use this plugin interface.'
+      ],
+      button: this.button
+    };
+  }
 }
 
 
