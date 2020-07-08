ccea1cbc05e1196a7aa8f4c514c6e1c8e275abe6
Ruel Mindo <wrelmind@gmail.com>
2020-07-09 00:22:10 +0800

    Removed spacing

--- src/views/forms.js
+++ src/views/forms.js
@@ -1,43 +1,42 @@
 class Forms {
 
-	constructor() {
-		this.head.title('Forms');
-	}
+  constructor() {
+    this.head.title('Forms');
+  }
 
 
-	button(node) {
-		node.id = 'button';
-		node.innerHTML = 'Click to see the world!';
-	}
+  button(node) {
+    node.id = 'button';
+    node.innerHTML = 'Click to see the world!';
+  }
 
 
-
-	content() {
-		return {
-			h1: 'Forms',
-			p: [
-				{
-					strong: 'webpack has a rich plugin interface.',
-					span: 'Test',
-					em: {
-						small: 'small',
-						span: {
-							strike: 'strike',
-							small: [
-								'one',
-								'two',
-								'three',
-								{
-									span: 'four'
-								}
-							]
-						}
-					}
-				}
-			],
-			button: this.button
-		};
-	}
+  content() {
+    return {
+      h1: 'Forms',
+      p: [
+        {
+          strong: 'webpack has a rich plugin interface.',
+          span: 'Test',
+          em: {
+            small: 'small',
+            span: {
+              strike: 'strike',
+              small: [
+                'one',
+                'two',
+                'three',
+                {
+                  span: 'four'
+                }
+              ]
+            }
+          }
+        }
+      ],
+      button: this.button
+    };
+  }
 }
 
 
