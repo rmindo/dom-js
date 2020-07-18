f9b1090267dbe432b8712e05d86d1e4fb567ef95
Ruel Mindo <wrelmind@gmail.com>
2020-07-19 00:54:15 +0800

    Removed spacing

--- src/views/automations.js
+++ src/views/automations.js
@@ -1,44 +1,44 @@
 class Automations {
 
-	constructor() {
-		this.head.title('Automations');
-	}
+  constructor() {
+    this.head.title('Automations');
+  }
 
 
-	button(node) {
-		node.id = 'button';
-		node.innerHTML = 'Click to see the world!';
-	}
+  button(node) {
+    node.id = 'button';
+    node.innerHTML = 'Click to see the world!';
+  }
 
 
-	content() {
-		return {
-			h1: 'Automations',
-			p: [
-				{
-					strong: 'webpack has a rich plugin interface.',
-					span: 'Test',
-					em: {
-						small: 'small',
-						span: {
-							strike: 'strike',
-							small: [
-								'one',
-								'two',
-								'three',
-								{
-									span: 'four'
-								}
-							]
-						}
-					}
-				},
-				'Most of the features within webpack itself use this plugin interface.',
-				'This makes webpack flexible.',
-			],
-			button: this.button
-		};
-	}
+  content() {
+    return {
+      h1: 'Automations',
+      p: [
+        {
+          strong: 'webpack has a rich plugin interface.',
+          span: 'Test',
+          em: {
+            small: 'small',
+            span: {
+              strike: 'strike',
+              small: [
+                'one',
+                'two',
+                'three',
+                {
+                  span: 'four'
+                }
+              ]
+            }
+          }
+        },
+        'Most of the features within webpack itself use this plugin interface.',
+        'This makes webpack flexible.',
+      ],
+      button: this.button
+    };
+  }
 }
 
 
